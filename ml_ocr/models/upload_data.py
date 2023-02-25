e27a1b9ef8f4d9efc5e7ea90c2cbfd6336b2f4d6
Novacept <94094993+NovaceptAI@users.noreply.github.com>
2023-02-25 23:52:36 +0530

    Update upload_data.py
    Changed MongoDb Credentials.

--- ml_ocr/models/upload_data.py
+++ ml_ocr/models/upload_data.py
@@ -2,22 +2,32 @@ import os
 import boto3
 from pymongo import MongoClient
 
-client = MongoClient(
-    "mongodb+srv://Novneet:jayhanuman1@cluster0.oxi79.mongodb.net/test?ssl=true&ssl_cert_reqs=CERT_NONE")
-db = client.DIGIMACHINE
+
+# db = client.DIGIMACHINE
 
 
-def upload_to_database(filename, upload_type=None):
-    data = db['creds'].find_one()
-    s3_bucket = 'digitalmachineocr'
-    s3 = boto3.resource(
-        service_name='s3',
-        region_name='ap-south-1',
-        aws_access_key_id=data['aws_access_key_id'],
-        aws_secret_access_key=data['aws_secret_access_key']
-    )
-    file_content = open(os.path.join('./tmp', filename), 'rb')
-    s3.Bucket(s3_bucket).put_object(Key=filename, Body=file_content)
-    object = s3.Bucket(s3_bucket).Object(filename)
-    # object.Acl().put(ACL='public-read')
-    return filename
+# def upload_to_database(filename, upload_type=None):
+#     data = db['creds'].find_one()
+#     s3_bucket = 'panrange'
+#     s3 = boto3.resource(
+#         service_name='s3',
+#         region_name='ap-south-1',
+#         aws_access_key_id=data['aws_access_key_id'],
+#         aws_secret_access_key=data['aws_secret_access_key']
+#     )
+#     file_content = open(os.path.join('./tmp', filename), 'rb')
+#     s3.Bucket(s3_bucket).put_object(Key=filename, Body=file_content)
+#     object = s3.Bucket(s3_bucket).Object(filename)
+#     # object.Acl().put(ACL='public-read')
+#     return filename
+
+
+def get_db():
+    client = MongoClient("mongodb://novacept:qhu5IVPqCjS3IQCI@ac-658ugvq-shard-00-00.wkfg1lx.mongodb.net:27017,"
+                         "ac-658ugvq-shard-00-01.wkfg1lx.mongodb.net:27017,"
+                         "ac-658ugvq-shard-00-02.wkfg1lx.mongodb.net:27017/?ssl=true&replicaSet=atlas-hg0dc7"
+                         "-shard-0&authSource=admin&retryWrites=true&w=majority")
+
+    # Create the database for our example (we will use the same database throughout the tutorial
+    db = client.DigiMachine
+    return db
