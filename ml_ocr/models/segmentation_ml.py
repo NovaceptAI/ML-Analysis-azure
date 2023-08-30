e1e1a0cddbb0e78a9d43897b33c67e4d14403688
Novneet Patnaik <novacept_ai@outlook.com>
2023-08-30 18:36:39 +0530

    Update segmentation_ml.py
    Re added Spacy

--- ml_ocr/models/segmentation_ml.py
+++ ml_ocr/models/segmentation_ml.py
@@ -1,19 +1,19 @@
 # import spacy library
-# import spacy
+import spacy
 import en_core_web_sm
 # from nltk.corpus import wordnet
 
 
 def segmentation_ml(output_data):
     # load core english library
-    # nlp = spacy.load("en_core_web_sm")
+    nlp = spacy.load("en_core_web_sm")
     # nlp = en_core_web_sm.load()
     # take unicode string
     # here u stands for unicode
-    # doc = nlp(output_data)
+    doc = nlp(output_data)
     # to print sentences
     print_list = []
-    # for sent in doc.sents:
-    #     print_list.append(sent)
+    for sent in doc.sents:
+        print_list.append(sent)
 
     return print_list
